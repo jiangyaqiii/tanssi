555407864c17a7f9107311f32bb2910b61c73ae1
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-06-17 23:36:51 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,37 +3,25 @@ if ! command -v docker &> /dev/null
 then
     # 如果 Docker 未安装，则进行安装
     echo "未检测到 Docker，正在安装..."
-    sudo apt-get install ca-certificates curl gnupg lsb-release -y
-
-    # 添加 Docker 官方 GPG 密钥
-    sudo mkdir -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-    # 设置 Docker 仓库
-    echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-    # 授权 Docker 文件
-    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    # Add Docker's official GPG key:
     sudo apt-get update -y
+    sudo apt-get install ca-certificates curl -y
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-    # 安装 Docker 最新版本
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+    # Add the repository to Apt sources:
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update -y
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 else
     echo "Docker 已安装。"
 fi
 
-# 安装 Docker compose 最新版本
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
-docker compose version
-
-# 验证 Docker Engine 安装是否成功
 sudo docker run hello-world
-# 应该能看到 hello-world 程序的输出
 
 docker pull moondancelabs/tanssi
 mkdir /var/lib/dancebox
