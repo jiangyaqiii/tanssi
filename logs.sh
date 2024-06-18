be9e1dce6df2a20df7dcd5d6fb243e4f084df103
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-06-18 15:40:04 +0800

    Update logs.sh

--- logs.sh
+++ logs.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-
-# 获取与 moondancelabs/tanssi 镜像关联的容器 ID
-container_id=$(docker ps -a --filter ancestor=moondancelabs/tanssi --format "{{.ID}")
-
-# 检查是否找到容器
-if [ ! -z "$container_id" ]; then
-    # 输出找到的容器 ID
-    echo "Found container ID: $container_id"
-    
-    # 查看容器日志
-    docker logs $container_id
-    
-else
-    echo "No containers found using moondancelabs/tanssi image."
-fi
+docker logs -f --tail=20 $(docker ps -a --filter ancestor=moondancelabs/tanssi --format "{{.ID}}")
